0224625453d294b40529e317a25485d1cea745cf
Nur Jamiludin Ramadhan <j.maill.mail@gmail.com>
2022-12-16 09:27:52 +0700

    logo changes

--- main.py
+++ main.py
@@ -12,16 +12,16 @@ from kivy.uix.image import Image
 from kivy.animation import Animation
 from kivy.clock import Clock
 from kivy.config import Config
+from kivy.metrics import dp
 from kivy.garden.matplotlib.backend_kivyagg import FigureCanvasKivyAgg
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 from matplotlib.figure import Figure
 from matplotlib.colors import ListedColormap, LinearSegmentedColormap
+from matplotlib.ticker import AutoMinorLocator
+from kivymd.uix.datatables import MDDataTable
 
 plt.style.use('bmh')
-
-Window.clearcolor = (.9, .9, .9, 1)
-Window.fullscreen = 'auto'
 
 colors = {
     "Red": {
@@ -47,9 +47,21 @@ colors = {
 from kivy.properties import ObjectProperty
 import time
 
+STEPS = 51
+MAX_DISTANCE = 100
+ELECTRODES_NUM = 48
+
+y_electrode = np.zeros((4, STEPS))
+x_electrode = np.zeros((4, STEPS))
+n_electrode = np.zeros((ELECTRODES_NUM, STEPS))
+c_electrode = np.array(["#FF0000","#FFDD00","#00FF00","#00FFDD"])
+l_electrode = np.array(["C1","C2","P1","P2"])
+
+
 class ScreenSplash(BoxLayout):
+# class ScreenSplash(Screen):
     screen_manager = ObjectProperty(None)
-    screen_main = ObjectProperty(None)
+    screen_setting = ObjectProperty(None)
     app_window = ObjectProperty(None)
     
     def __init__(self, **kwargs):
@@ -68,26 +80,27 @@ class ScreenSplash(BoxLayout):
             self.ids.progress_bar.value = 100
             self.ids.progress_bar_label.text = 'Loading.. [{:} %]'.format(100)
             time.sleep(0.5)
-            self.screen_manager.current = 'screen_main'
+            self.screen_manager.current = 'screen_setting'
             return False
 
-class ScreenMain(BoxLayout):
+class ScreenSetting(BoxLayout):
     screen_manager = ObjectProperty(None)
-    app_window = ObjectProperty(None)
 
     checks_mode = []
     checks_config = []
-    dt_mode = ""
-    dt_config = ""
 
     flag_measure = False
 
+    dt_mode = ""
+    dt_config = ""
+
     dt_distance = 0
+    dt_constant = 10
     dt_time = 0
-    dt_step = 10
+    dt_step = 0
 
     def __init__(self, **kwargs):
-        super(ScreenMain, self).__init__(**kwargs)
+        super(ScreenSetting, self).__init__(**kwargs)
 
     def hide_widget(self, wid, dohide=True):
         if hasattr(wid, 'saved_attrs'):
@@ -105,41 +118,82 @@ class ScreenMain(BoxLayout):
         self.hide_widget(self.ids.layout_illustration, False)
 
         self.dt_distance = self.ids.slider_distance.value
+        self.dt_constant = self.ids.slider_constant.value
         self.dt_time = self.ids.slider_time.value
-        self.dt_step = self.ids.slider_step.value
-
-        offset = (self.dt_time + self.dt_distance) / 2
-        amplitude = (self.dt_time - self.dt_distance) / 2
-
-        x_axis = np.linspace(0, 100, int(10000/self.dt_step), endpoint=True) #20 data per periode in 100ms axis
-
-        if("(SP) SELF POTENTIAL" in self.dt_mode):
-            y_axis = offset + (4 * amplitude / self.dt_step) * np.abs((((x_axis - self.dt_step / 4) % self.dt_step) + self.dt_step) % self.dt_step - self.dt_step / 2 ) - amplitude
-            data_adc = np.round(0.0 + (1.0 - 0.0) * ((y_axis - 160) / (360 - 160)) , 2)
-            # print(y_axis)
-        elif("(IP) INDUCED POLARIZATION" in self.dt_mode):
-            y_axis = offset + (amplitude * np.sign (np.sin(2 * np.pi * 1 / self.dt_step * x_axis)))
-            data_adc = np.round(0.0 + (1.0 - 0.0) * ((y_axis - 160) / (360 - 160)) , 2)
-            # print(y_axis)
-        elif("(R) RESISTIVITY" in self.dt_mode):
-            y_axis = self.dt_distance + ((2 * amplitude / self.dt_step) * (x_axis % self.dt_step))
-            data_adc = np.round(0.0 + (1.0 - 0.0) * ((y_axis - 160) / (360 - 160)) , 2)
-            # print(y_axis)
-        elif("(R+IP) COMBINATION" in self.dt_mode):
-            y_axis = self.dt_distance + ((2 * amplitude / self.dt_step) * (x_axis % self.dt_step))
-            data_adc = np.round(0.0 + (1.0 - 0.0) * ((y_axis - 160) / (360 - 160)) , 2)
-            # print(y_axis)
-        else:
-            y_axis = 0 * x_axis
-            data_adc = y_axis
+        self.dt_step = int(self.ids.slider_step.value)
 
         self.fig1, self.ax = plt.subplots()
-        self.fig1.set_facecolor((.9,.9,.9))
-        self.ax.set_facecolor((.9,.9,.9))
-        self.ax.plot(x_axis, y_axis)
+        self.fig1.set_facecolor("#eeeeee")
+        self.fig1.tight_layout(pad=3.0)
+
+        # x_electrode = np.zeros((4,STEPS))
+        self.ids.layout_illustration.remove_widget(FigureCanvasKivyAgg(self.fig1))
+
+        if("WENNER" in self.dt_config):
+            x_electrode[0, self.dt_step] = self.dt_distance * self.dt_step
+            x_electrode[1, self.dt_step] = self.dt_distance + x_electrode[0, self.dt_step]
+            x_electrode[2, self.dt_step] = self.dt_distance + x_electrode[1, self.dt_step]
+            x_electrode[3, self.dt_step] = self.dt_distance + x_electrode[2, self.dt_step]
+            self.ax.set_xlim([-2, MAX_DISTANCE])
+
+        elif("SCHLUMBERGER" in self.dt_config):
+            x_electrode[0, self.dt_step] = -0.5 *  self.dt_distance
+            x_electrode[1, self.dt_step] = 0.5 * self.dt_distance
+            x_electrode[2, self.dt_step] = -0.5 * self.dt_distance * self.dt_constant
+            x_electrode[3, self.dt_step] = 0.5 * self.dt_distance * self.dt_constant
+            self.ax.set_xlim([-MAX_DISTANCE/2, MAX_DISTANCE/2])
+
+        elif("DIPOLE-DIPOLE" in self.dt_config):
+            x_electrode[0, self.dt_step] = 0
+            x_electrode[1, self.dt_step] = self.dt_distance + x_electrode[0, self.dt_step]
+            x_electrode[2, self.dt_step] = self.dt_constant + x_electrode[1, self.dt_step]
+            x_electrode[3, self.dt_step] = self.dt_distance + x_electrode[2, self.dt_step]
+            self.ax.set_xlim([-2, MAX_DISTANCE])
+
+        else:
+            pass
+
+        if("(SP) SELF POTENTIAL" in self.dt_mode):
+            n_electrode[0, self.dt_step] = 0 #none
+            n_electrode[1, self.dt_step] = 0 #none
+            n_electrode[2, self.dt_step] = 3 #p1
+            n_electrode[3, self.dt_step] = 4 #p2
+
+        elif("(IP) INDUCED POLARIZATION" in self.dt_mode):
+            n_electrode[0, self.dt_step] = 1 #c1
+            n_electrode[1, self.dt_step] = 2 #c2
+            n_electrode[2, self.dt_step] = 3 #p1
+            n_electrode[3, self.dt_step] = 4 #p2
+
+        elif("(R) RESISTIVITY" in self.dt_mode):
+            n_electrode[0, self.dt_step] = 1 #c1
+            n_electrode[1, self.dt_step] = 2 #c2
+            n_electrode[2, self.dt_step] = 3 #p1
+            n_electrode[3, self.dt_step] = 4 #p2
+
+        elif("(R+IP) COMBINATION" in self.dt_mode):
+            n_electrode[0, self.dt_step] = 1 #c1
+            n_electrode[1, self.dt_step] = 2 #c2
+            n_electrode[2, self.dt_step] = 3 #p1
+            n_electrode[3, self.dt_step] = 4 #p2
+            
+        else:
+            pass
+
+        self.ax.set_facecolor("#eeeeee")
+        self.ax.scatter(x_electrode[0, self.dt_step], y_electrode[0, self.dt_step], c=c_electrode[0], label=l_electrode[0], marker=7, s=100)
+        self.ax.scatter(x_electrode[1, self.dt_step], y_electrode[1, self.dt_step], c=c_electrode[1], label=l_electrode[1], marker=7, s=100)
+        self.ax.scatter(x_electrode[2, self.dt_step], y_electrode[2, self.dt_step], c=c_electrode[2], label=l_electrode[2], marker=7, s=100)
+        self.ax.scatter(x_electrode[3, self.dt_step], y_electrode[3, self.dt_step], c=c_electrode[3], label=l_electrode[3], marker=7, s=100)
+        self.ax.legend(loc='center left', bbox_to_anchor=(1, 0.5), title="Electrode")
+        self.ax.set_xlabel('distance (m)')
+        # self.ax.xaxis.set_minor_locator(AutoMinorLocator(2))
+        # self.ax.yaxis.set_minor_locator(AutoMinorLocator(2))
+        self.ax.grid(which='minor', linewidth=0.6, color="#000000")
 
         self.ids.layout_illustration.clear_widgets()
         self.ids.layout_illustration.add_widget(FigureCanvasKivyAgg(self.fig1))
+        print(n_electrode)
 
     def measure(self):
         if(self.flag_measure):
@@ -188,42 +242,73 @@ class ScreenMain(BoxLayout):
         self.dt_config = configs
 
     def screen_setting(self):
-        self.hide_widget(self.ids.layout_setting, False)
-        self.hide_widget(self.ids.layout_data)
-        self.hide_widget(self.ids.layout_graph)
-
-        self.hide_widget(self.ids.layout_illustration)
+        self.screen_manager.current = 'screen_setting'
 
     def screen_data(self):
-        self.hide_widget(self.ids.layout_setting)
-        self.hide_widget(self.ids.layout_data, False)
-        self.hide_widget(self.ids.layout_graph)
-
-        self.hide_widget(self.ids.layout_illustration)
-
-        self.ids.layout_data.opacity = 1
-        self.ids.layout_data.size_hint_x = 1
-        self.ids.layout_data.size_hint_y = 0.5
+        self.screen_manager.current = 'screen_data'
 
     def screen_graph(self):
-        self.hide_widget(self.ids.layout_setting)
-        self.hide_widget(self.ids.layout_data)
-        self.hide_widget(self.ids.layout_graph, False)
+        self.screen_manager.current = 'screen_graph'
 
-        self.hide_widget(self.ids.layout_illustration)
+class ScreenData(BoxLayout):
+    screen_manager = ObjectProperty(None)
 
-        self.ids.layout_graph.opacity = 1
-        self.ids.layout_graph.size_hint_x = 1
-        self.ids.layout_graph.size_hint_y = 0.5
+    def __init__(self, **kwargs):
+        super(ScreenData, self).__init__(**kwargs)
+
+    def data(self):
+        layout = self.ids.layout_tables
+        
+        data_tables = MDDataTable(
+            use_pagination=True,
+            column_data=[
+                ("No.", dp(30)),
+                ("Column 1", dp(30)),
+                ("Column 2", dp(30)),
+                ("Column 3", dp(30)),
+                ("Column 4", dp(30)),
+                ("Column 5", dp(30)),
+            ],
+            row_data=[
+                (f"{i + 1}", "1", "2", "3", "4", "5") for i in range(50)
+            ],
+        )
+
+        layout.remove_widget(data_tables)
+        layout.add_widget(data_tables)
+
+    def screen_setting(self):
+        self.screen_manager.current = 'screen_setting'
+
+    def screen_data(self):
+        self.screen_manager.current = 'screen_data'
+
+    def screen_graph(self):
+        self.screen_manager.current = 'screen_graph'
+
+class ScreenGraph(BoxLayout):
+    screen_manager = ObjectProperty(None)
+
+    def __init__(self, **kwargs):
+        super(ScreenGraph, self).__init__(**kwargs)
+
+    def screen_setting(self):
+        self.screen_manager.current = 'screen_setting'
+
+    def screen_data(self):
+        self.screen_manager.current = 'screen_data'
+
+    def screen_graph(self):
+        self.screen_manager.current = 'screen_graph'
 
 class BSDSApp(MDApp):
     def build(self):
-        # SplashScreen.resize_window(400, 300)
         self.theme_cls.colors = colors
-        # self.theme_cls.theme_style = "Light"
         self.theme_cls.primary_palette = "Blue"
+        Window.fullscreen = 'auto'
         Window.borderless = True
         Window.allow_screensaver = True
+        self.icon = 'asset/logo_labtek_p.ico'
 
         screen = Builder.load_file('main.kv')
 
